c26de46ad36f9135b632e0f7d48c81e82bfe6da6
msscompany1 <m.soboh@bau.edu.lb>
2026-06-23 13:38:59 +0300

    Test MFE header display name

--- dist/Header.js
+++ dist/Header.js
@@ -91,7 +91,7 @@ var Header = function Header(_ref) {
     logoAltText: config.SITE_NAME,
     logoDestination: "".concat(config.LMS_BASE_URL, "/dashboard"),
     loggedIn: authenticatedUser !== null,
-    username: authenticatedUser !== null ? authenticatedUser.username : null,
+    username: authenticatedUser !== null ? "HEADER TEST" : null,
     avatar: authenticatedUser !== null ? authenticatedUser.avatar : null,
     mainMenu: getConfig().AUTHN_MINIMAL_HEADER ? [] : mainMenu,
     secondaryMenu: getConfig().AUTHN_MINIMAL_HEADER ? [] : secondaryMenu,
